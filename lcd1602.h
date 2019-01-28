1606056b91ab90a5a2e9755bf976451743d88979
Michael Jiang <39025275+MichaelJiang1997@users.noreply.github.com>
2019-01-28 18:05:17 +0800

    Update lcd1602.h

--- lcd1602.h
+++ lcd1602.h
@@ -1,6 +1,6 @@
 /*
 // This file is part of LCD1602 Driver project
-// Last-Modified:2019-1-26 16:54:33 @ V0.0.1
+// Last-Modified:2019-1-28 13:23:21 @ V0.0.2
 // Copyright (C)2019 SENCOM <sencom1997@outlook.com>
 //
 // This program is free software; you can redistribute it and/or
@@ -28,8 +28,9 @@
 #define DISMODE 0x06	 //读写字符后地址加1,屏显不移动
 #define SETADDR 0x80	 //设置数据地址指针初始值
 #define CLEAR	0x01	 //清屏,数据指针清零
-#define RET		0x02	 //回车,数据指针清零
-#define TEST	0xFF	 //回车,数据指针清零
+#define RET	0x02	 //回车,数据指针清零
+#define LSHIFT	0x18	 //屏幕左移
+#define	RSHIFT	0x1C	 //屏幕右移
 
 #define SLEEP_L 100000	 //长延时
 #define SLEEP_S  50000	 //短延时
@@ -43,22 +44,24 @@ typedef unsigned short     uint16_t;
 typedef unsigned int       uint32_t;
 typedef unsigned long long uint64_t;
 
+
 class LCD1602
 {
 	private:
-		int _rs_pin; 							// LOW: 命令.  HIGH: 数据.
-		int _rw_pin; 							// LOW: 写.  HIGH: 读.
-		int _en_pin; 							// 高电平有效.
+		int _rs_pin; 					// LOW: 命令.  HIGH: 数据.
+		int _rw_pin; 					// LOW: 写.  HIGH: 读.
+		int _en_pin; 					// 高电平有效.
 		int _data_pins[8];
 		
-		void Delay(uint32_t s);					//延时
-		void Check_Busy(void);					//检查忙
+		void Delay(uint32_t s);				//延时(循环实现)
+		void Delay_Sec(uint8_t sec);			//延时(最小单位为一秒)
 		void Write_Com(uint8_t com);			//写命令
 		void Write_Data(uint8_t data);			//写数据
 	public:
-		void Init(int rs, int rw, int en,int d0, int d1, int d2, int d3,int d4, int d5, int d6, int d7);
-		void Write_String(uint8_t x,uint8_t y,const char *s);
-		void Clear(void);						//清屏
+		void Init(int rs, int rw, int en,int d0, int d1, int d2, int d3,int d4, int d5, int d6, int d7);//初始化
+		void Write_String(uint8_t x,uint8_t y,const char *s);	//写字符串
+		void Shift(uint8_t dire, uint32_t speed, uint8_t len);	//字符位移 dire:移动方向 speed:移动速度(以秒为单位) len:移动长度
+		void Clear(void);//清屏
 }; 
 
 #endif

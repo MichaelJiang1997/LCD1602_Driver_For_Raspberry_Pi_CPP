8df35b89b39ec87a6925a2978fdda8147e660993
Michael Jiang <39025275+MichaelJiang1997@users.noreply.github.com>
2019-01-28 18:04:23 +0800

    Update lcd1602.cpp

--- lcd1602.cpp
+++ lcd1602.cpp
@@ -1,6 +1,6 @@
 /*
 // This file is part of LCD1602 Driver project
-// Last-Modified:2019-1-26 16:54:33 @ V0.0.1
+// Last-Modified:2019-1-28 13:23:21 @ V0.0.2
 // Copyright (C)2019 SENCOM <sencom1997@outlook.com>
 //
 // This program is free software; you can redistribute it and/or
@@ -42,13 +42,9 @@ void LCD1602::Init( int rs, int rw, int en, int d0, int d1, int d2, int d3, int 
 	pinMode(_rs_pin,OUTPUT);
 	
 	Write_Com(SETMODE);     //模式设置
-    Delay(SLEEP_L);
-    Write_Com(DISOPEN);     //显示设置
-    Delay(SLEEP_L);
-    Write_Com(DISMODE);     //显示模式
-    Delay(SLEEP_L);
-    Write_Com(CLEAR);       //清屏
-	Delay(SLEEP_L);
+    	Write_Com(DISOPEN);     //显示设置
+    	Write_Com(DISMODE);     //显示模式
+    	Write_Com(CLEAR);       //清屏
 	
 }
 
@@ -57,33 +53,18 @@ void LCD1602::Write_Com(uint8_t com)
 	digitalWrite(_en_pin,LOW);
 	digitalWrite(_rs_pin,LOW);
 	digitalWrite(_rw_pin,LOW);
-//	printf("%2X  ",com);
 	for(uint8_t i=0; i<8; i++)
 		digitalWrite(_data_pins[i],(com >> i) & 0x01);
-//	for(uint8_t i=0; i<8; i++)				刚开始时候老是初始化失败(没反应)
-//	{							以为是这里宏定义 HIGH LOW 的问题
-//		if ( (com >> i) & 0x01 == 1)			后来去看wiringPi.h 发现
-//		{						HIGH 定义为 1
-//			digitalWrite(_data_pins[i],HIGH);	LOW  定义为 0  完全没毛病
-//			printf("1 ");				过了好几天后重新拾起,检查发现是线接错了... ...
-//		}						所以就留下了这一段Debug代码
-//		else
-//		{
-//			digitalWrite(_data_pins[i],LOW);
-//			printf("0 ");
-//		}
-//	}
-//	printf("\n");
 	Delay(SLEEP_S);
 	digitalWrite(_en_pin,HIGH);
 	Delay(SLEEP_S);
 	digitalWrite(_en_pin,LOW);
+	Delay(SLEEP_L);
 }
 
 void LCD1602::Write_Data(uint8_t data)
 {
 	digitalWrite(_en_pin,LOW);
-	Delay(SLEEP_S);
 	digitalWrite(_rs_pin,HIGH);
 	digitalWrite(_rw_pin,LOW);
 	for(uint8_t i=0; i<8; i++)
@@ -92,9 +73,11 @@ void LCD1602::Write_Data(uint8_t data)
 	digitalWrite(_en_pin,HIGH);
 	Delay(SLEEP_S);
 	digitalWrite(_en_pin,LOW);
+	Delay(SLEEP_S);
 }
 void LCD1602::Write_String(uint8_t x,uint8_t y,const char *s)
 {
+	Write_Com(0x80);				//消除第一次不能指定位置写入问题
 	if(y == 1)
 		Write_Com(0x80 + x - 1);		//在第一行 x 列写
 	else
@@ -109,15 +92,38 @@ void LCD1602::Write_String(uint8_t x,uint8_t y,const char *s)
 void LCD1602::Clear()
 {
 	Write_Com(CLEAR);
-	Delay(SLEEP_L);
+}
+
+void LCD1602::Shift(uint8_t dire, uint32_t speed, uint8_t len)
+{
+	if( dire )
+	{
+		for(int i = 0; i < len; i++)
+		{
+			Write_Com(LSHIFT);
+			Delay_Sec(speed);
+		}
+	}
+	else
+	{
+		for(int i = 0; i < len; i++)
+		{
+			Write_Com(RSHIFT);
+			Delay_Sec(speed);
+		}
+	}
+}
+
+void LCD1602::Delay_Sec(uint8_t sec)
+{
+	clock_t start = clock();
+	clock_t lay = (clock_t)sec * CLOCKS_PER_SEC;
+	while ((clock()-start) < lay);
 }
 
 void LCD1602::Delay(uint32_t s)
 {
-//	此方法延时只能按秒计算 过长
-//	clock_t start = clock();
-//	clock_t lay = (clock_t)s * CLOCKS_PER_SEC;
-//	while ((clock()-start) < lay);
-	while(s)s --;						//现在换成 while 循环
+	while(s)s --;
 }
+
 
